70ee96bd5d7b984ce38ad48214fedef46250503d
hungdev <domanhhungit1@gmail.com>
2023-10-29 17:31:06 +0700

    even

--- app/src/main/java/com/example/hikeapp/MainActivity.java
+++ app/src/main/java/com/example/hikeapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.hikeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_info,btn_hike,btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        link();
+        event();
+    }
+
+    private void event() {
+        Intent intent;
+        intent = new Intent(this,AllHikeAcivity.class);
+        btn_hike.setOnClickListener(view -> {
+            startActivity(intent);
+        });
+    }
+
+    private void link() {
+        btn_add = findViewById(R.id.btn_addHike);
+        btn_hike= findViewById(R.id.btn_hike);
+        btn_info = findViewById(R.id.btn_information);
     }
 }
